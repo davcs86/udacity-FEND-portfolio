bdf39164c0ffd52817f66d993b936d5f4f32e26e
davcs86 <davcs86@gmail.com>
2015-11-05 02:07:17 -0600

    FEAT: Markup, basic style and polymer for portfolio items. CHORE: Unify tasks in the "default" task, remove unnecessary tasks

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,33 @@
-console.log('\'Allo \'Allo!'); // eslint-disable-line no-console
+var App = {
+    $portfolioContainer: null,
+    $data: null,
+    Init: function(){
+        App.$portfolioContainer = $('#portfolio-container');
+        App.$data = [
+            {
+                itemId: 'project1',
+                itemImgUrlBase : 'images/project1',
+                itemName : 'Project #1',
+                itemUrl : 'http://github.com/davcs86/fakeproject1',
+                itemDescription : 'AAAAA'
+            },
+            {
+                itemId: 'project2',
+                itemImgUrlBase : 'images/project2',
+                itemName : 'Project #2',
+                itemUrl : 'http://github.com/davcs86/fakeproject2',
+                itemDescription : 'AAAAA'
+            }
+        ];
+        App.Render();
+    },
+    Render: function(){
+        for (var i in App.$data){
+            var portfolioItem = $('<portfolio-item/>');
+            portfolioItem.attr(App.$data[i]);
+            App.$portfolioContainer.append(portfolioItem);
+        }
+    }
+};
+
+App.Init();
